2e1cff2ab88b4b127a0c99c095f0a6a60cba30a4
Max <maksim.shaggy@gmail.com>
2026-05-10 21:09:37 +0300

    add solution

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,25 @@ import './App.scss';
 import { MoviesList } from './components/MoviesList';
 import { NewMovie } from './components/NewMovie';
 import moviesFromServer from './api/movies.json';
+import { useState } from 'react';
+import { Movie } from './types/Movie';
 
 export const App = () => {
+  const [movies, setMovies] = useState<Movie[]>(moviesFromServer);
+  const [formKey, setFormKey] = useState(0);
+
+  const handleAddMovie = (newMovie: Movie) => {
+    setMovies(currentMovies => [...currentMovies, newMovie]);
+    setFormKey(prevKey => prevKey + 1);
+  };
+
   return (
     <div className="page">
       <div className="page-content">
-        <MoviesList movies={moviesFromServer} />
+        <MoviesList movies={movies} />
       </div>
       <div className="sidebar">
-        <NewMovie /* onAdd={(movie) => {}} */ />
+        <NewMovie key={formKey} onAdd={handleAddMovie} />
       </div>
     </div>
   );
